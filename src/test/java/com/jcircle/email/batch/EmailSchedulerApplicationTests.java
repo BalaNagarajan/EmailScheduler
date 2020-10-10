435f1bb3f9e16bee371b821e150221c85a738a08
Bala Nagarajan <Bala.Nagarajan@boydgroup.com>
2020-10-10 11:55:17 -0500

    Initial Changes

---
+++ src/test/java/com/jcircle/email/batch/EmailSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jcircle.email.batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
